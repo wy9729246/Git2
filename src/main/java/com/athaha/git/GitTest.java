01709317515788d9b206964ddec15c1b4de1d4a6
wanghh <1018875716@qq.com>
2022-07-19 19:59:38 +0800

    hot-fix c2

--- src/main/java/com/athaha/git/GitTest.java
+++ src/main/java/com/athaha/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helle,git");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
