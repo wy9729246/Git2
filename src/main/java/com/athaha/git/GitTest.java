096aa6116c12d9cf3a0904463c2335769794dd52
wy9729246 <72370525+wy9729246@users.noreply.github.com>
2022-07-19 20:15:27 +0800

    Update GitTest.java

--- src/main/java/com/athaha/git/GitTest.java
+++ src/main/java/com/athaha/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
