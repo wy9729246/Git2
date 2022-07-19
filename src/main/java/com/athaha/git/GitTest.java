d655329e6f70372612f1b5fb883423df50e6fedc
wanghh <1018875716@qq.com>
2022-07-19 17:49:36 +0800

    first c

---
+++ src/main/java/com/athaha/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.athaha.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author WangHHH
+ * @version 1.0
+ * @className Java
+ * @description
+ * @date 2022/7/19 17:46
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helle,git");
+    }
+}
